7c5bc40a8333279f2981d9696de32008c154eba3
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-06 14:26:43 -0400

    Checking for wall and ceiling collisions

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0 #perfect height compared to mario bros is 500 :)
+## other height incluce 555 for running and 610? for extra
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,14 +13,24 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	if is_on_ceiling_only():
+		$Sprite2D.modulate = Color(1,0,0)
+		print("Real: " + str(get_real_velocity()))
+		print("Normal: " + str(velocity))
+	else:
+		$Sprite2D.modulate = Color(1,1,1)
+		
+	if is_on_wall():
+		$Sprite2D.modulate = Color(0,1,0)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
