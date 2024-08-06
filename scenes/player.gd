a5c3d92731c4849bcfe8029399493cbaedb580af
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-06 17:01:31 -0400

    Added Nintendos Very Own, animated and all
    Mario is animated with movement, needs some tweaking. Jumps have also been calculated to have an accurate height as in mario games and a letgo mechanic which lets mario jump lower than usual if you letgo early. Overall a nice change. Will add Tilesets and make the running and jumping accurate to mario games (if not the nes version)

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,37 +2,63 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0 #perfect height compared to mario bros is 500 :)
-## other height incluce 555 for running and 610? for extra
+const JUMP_VELOCITY = -491.7 #perfect height compared to mario bros :)
+## other height incluce 500 for walking, 555 for running and 610 for Special running
+
+## ABSOLUTE MINIMUMS ARE 491.7, 551, 606
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func on_ready():
+	$AnimatedSprite2D.play("default")
 
 func _physics_process(delta):
 	# Add the gravity.
+	if is_on_floor():
+		if velocity.x == 0:
+			$AnimatedSprite2D.play("default")
+
+		
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$AnimatedSprite2D.stop()
 	
 	if is_on_ceiling_only():
-		$Sprite2D.modulate = Color(1,0,0)
+		$AnimatedSprite2D.modulate = Color(1,0,0)
 		print("Real: " + str(get_real_velocity()))
 		print("Normal: " + str(velocity))
 	else:
-		$Sprite2D.modulate = Color(1,1,1)
+		$AnimatedSprite2D.modulate = Color(1,1,1)
 		
 	if is_on_wall():
-		$Sprite2D.modulate = Color(0,1,0)
+		$AnimatedSprite2D.modulate = Color(0,1,0)
+
+	if Input.is_action_pressed("left") and is_on_floor():
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("right") and is_on_floor():
+		$AnimatedSprite2D.flip_h = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		
+		
+	if Input.is_action_just_released("jump") and is_on_floor() == false:
+		if velocity.y < -125:
+			velocity.y = -125
+		elif velocity.y < 0:
+			velocity.y = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
+
 	if direction:
 		velocity.x = direction * SPEED
+		if not $AnimatedSprite2D.is_playing() and is_on_floor():
+			$AnimatedSprite2D.play("move")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
