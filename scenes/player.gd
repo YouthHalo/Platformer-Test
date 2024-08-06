864f6a1e9510af9fa68be7047c0e1486b0b94852
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-06 18:35:48 -0400

    running and run jumps added

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -491.7 #perfect height compared to mario bros :)
-## other height incluce 500 for walking, 555 for running and 610 for Special running
 
-## ABSOLUTE MINIMUMS ARE 491.7, 551, 606
+#Jump heights are 491.7, 551, 606 for standing, running and special run
+##these are with a not so accurate hitbox, might fix later
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var speedMulti = 1
 
 func on_ready():
 	$AnimatedSprite2D.play("default")
@@ -34,18 +36,20 @@ func _physics_process(delta):
 	if is_on_wall():
 		$AnimatedSprite2D.modulate = Color(0,1,0)
 
-	if Input.is_action_pressed("left") and is_on_floor():
-		$AnimatedSprite2D.flip_h = true
-	elif Input.is_action_pressed("right") and is_on_floor():
+	if Input.is_action_pressed("right") and is_on_floor():
 		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("left") and is_on_floor():
+		$AnimatedSprite2D.flip_h = true
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY - abs(velocity.x*0.25)
 		$AnimatedSprite2D.play("jump")
 		
 		
 	if Input.is_action_just_released("jump") and is_on_floor() == false:
+		if velocity.y < -250:
+			velocity.y = -250
 		if velocity.y < -125:
 			velocity.y = -125
 		elif velocity.y < 0:
@@ -55,11 +59,25 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 
+	
+	if Input.is_action_pressed("run") and is_on_floor():
+		$AnimatedSprite2D.speed_scale = 2
+		if speedMulti < 2:
+			speedMulti += 0.05
+		
+	elif Input.is_action_just_released("run"):
+		$AnimatedSprite2D.speed_scale = 1
+		if speedMulti > 1:
+			speedMulti -= 0.05
+	if velocity.x == 0:
+		speedMulti = 1
+		
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * speedMulti
 		if not $AnimatedSprite2D.is_playing() and is_on_floor():
 			$AnimatedSprite2D.play("move")
+
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * speedMulti)
 
 	move_and_slide()
